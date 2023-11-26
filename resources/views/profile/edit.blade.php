54a22864fead22519c6eec3e41e10a688cb4b57e
DiogoCortezRocha <Diogocortez@ua.pt>
2023-11-26 16:49:47 +0000

    alterçao de imagen

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -75,7 +75,7 @@
                             <div class="block block-three"></div>
                             <div class="block block-four"></div>
                             <a href="#">
-                                <img class="avatar" src="{{ asset('white') }}/img/anime3.png" alt="">
+                                <img class="avatar" src="{{ asset('white') }}/img/default-avatar.png" alt="">
                                 <h5 class="title">{{ auth()->user()->name }}</h5>
                             </a>
                             <p class="description">
